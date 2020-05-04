eeb9cf3d704d18534f9a59b60e2b32b5762cc073
Abhijeet Rastogi <arastogi@linkedin.com>
2020-05-03 17:35:44 -0700

    Update configs

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,6 +1,9 @@
 require "window"
 require "tiling"
 
+-- Enable Apple Script support
+hs.allowAppleScript(true)
+
 -- Load spoons
 hs.loadSpoon("ReloadConfiguration")
 spoon.ReloadConfiguration:start()
