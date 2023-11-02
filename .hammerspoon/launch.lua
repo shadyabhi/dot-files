ff2799a5886dae7e33c9ffc63f1302b5e0b6c668
Abhijeet Rastogi <arastogi@linkedin.com>
2023-11-02 11:44:05 -0700

    sync

--- .hammerspoon/launch.lua
+++ .hammerspoon/launch.lua
@@ -17,6 +17,11 @@ function launchApp(name)
             hs.application.launchOrFocus(path)
             return
         end
+        path = "/Users/arastogi/Applications/Edge Apps.localized/" .. name .. ".app"
+        if file_exists(path) then
+            hs.application.launchOrFocus(path)
+            return
+        end
         path = "/System/Library/CoreServices/" .. name .. ".app"
         if file_exists(path) then
             hs.application.launchOrFocus(path)
@@ -27,13 +32,27 @@ function launchApp(name)
             hs.application.launchOrFocus(path)
             return
         end
+        path = "/System/Applications/" .. name .. ".app"
+        if file_exists(path) then
+            hs.application.launchOrFocus(path)
+            return
+        end
+		path = "/Users/arastogi/Applications/Chrome Apps.localized/" .. name .. ".app"
+        if file_exists(path) then
+            hs.application.launchOrFocus(path)
+            return
+        end
     end
 end
 
 -- Shortcuts
-h_bind("c", launchApp("Google Chrome"))
+h_bind("b", launchApp("Google Chrome"))
 h_bind("v", launchApp("Visual Studio Code"))
 h_bind("t", launchApp("iTerm"))
-h_bind("w", launchApp("Typora"))
+h_bind("m", launchApp("Microsoft Teams"))
+h_bind("w", launchApp("Obsidian"))
 h_bind("s", launchApp("Slack"))
-h_bind("n", launchApp("Notion"))
+h_bind("o", launchApp("Microsoft Outlook"))
+h_bind("d", launchApp("DevDocs"))
+h_bind("f", launchApp("Firefox"))
+h_bind("r", launchApp("Reclaim"))
