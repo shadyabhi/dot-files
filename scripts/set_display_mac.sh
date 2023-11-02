3859d7cdd4aa1276fe987e2884f4e8a47eefddbc
Abhijeet Rastogi <arastogi@linkedin.com>
2023-11-02 11:36:03 -0700

    sync

---
+++ scripts/set_display_mac.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#displayplacer "id:6090C5F5-08C9-E837-468F-31FDF2D66022 res:2048x1280 hz:59 color_depth:4 scaling:on origin:(0,0) degree:0"
+#displayplacer "id:5EF76A10-0205-0F33-9F09-521C99FF40B1 res:4096x2304 hz:60 color_depth:8 scaling:off origin:(0,0) degree:0"
+displayplacer "id:5EF76A10-0205-0F33-9F09-521C99FF40B1+6090C5F5-08C9-E837-468F-31FDF2D66022 res:3840x2160 hz:60 color_depth:8 enabled:true scaling:off origin:(0,0) degree:0"
